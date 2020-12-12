47ff81e7bd1aa6ec1b8fea9cba2c69b02578b58a
Darren Mulholland <dmulholl@tcd.ie>
2020-12-12 17:41:19 +0000

    Bump version to 2.0.0-rc3

--- ibis/__init__.py
+++ ibis/__init__.py
@@ -8,7 +8,7 @@ from .template import Template
 
 
 # Library version.
-__version__ = "2.0.0-rc2"
+__version__ = "2.0.0-rc3"
 
 
 # Assign a template-loading callable here to enable the {% include %} and {% extends %} tags.
